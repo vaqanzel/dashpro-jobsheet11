d90c7e3b8b84df25d88e2d514b0c59060d39e9a8
vaqanzel <valengrehasta@gmail.com>
2023-11-09 04:14:56 +0700

    Mengubah i=1 menjadi i=0

--- Square.java
+++ Square.java
@@ -5,8 +5,8 @@ public class Square {
     Scanner sc = new Scanner(System.in);
        System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
-        for(int iOuter=0; iOuter<=N; iOuter++){
-        for(int i=1; i<=N; i++){
+        for(int iOuter=1; iOuter<=N; iOuter++){
+        for(int i=0; i<=N; i++){
         System.out.print("*");
        }
        System.out.println();
