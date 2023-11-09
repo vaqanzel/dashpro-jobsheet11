cc84c1a6e877e932c6be4b489b22569a8dd4ce63
vaqanzel <valengrehasta@gmail.com>
2023-11-10 00:36:45 +0700

    mencetak tampilan segitiga angka

---
+++ Tugas.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Tugas {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N: ");
+        int N = scanner.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int k = N; k > i; k--) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
