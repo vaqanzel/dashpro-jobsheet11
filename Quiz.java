c76d32945d47c68eba14f693ce125a9e9817e626
vaqanzel <valengrehasta@gmail.com>
2023-11-09 23:27:32 +0700

    Menambahkan kode tidak mengulangi permainan

--- Quiz.java
+++ Quiz.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 import java.util.Random;
 
@@ -6,14 +5,32 @@ public class Quiz {
     public static void main(String[] args) {
         Random random = new Random(); 
         Scanner input = new Scanner(System.in);
-        char menu='y'; do{
-            int number = random.nextInt(10) + 1; boolean success = false;
-            do {
-            System.out.print("Tebak angka (1-10): "); int answer = input.nextInt(); input.nextLine();
-            success = (answer == number);
-            } while(!success);
-            System.out.print("Apakah Anda ingin mengulang permainan (Y/y)?"); menu = input.nextLine().charAt(0);
-            } while(menu=='y' || menu=='Y');
+        char menu = 'y'; 
+        
+        do {
+            int number = random.nextInt(10) + 1; 
+            boolean success = false;
             
+            do {
+                System.out.print("Tebak angka (1-10): ");
+                int answer = input.nextInt(); 
+                input.nextLine();
+                success = (answer == number);
+            } while(!success);
+            
+            System.out.print("Apakah Anda ingin mengulang permainan? (Y/y, T/t): "); 
+            menu = input.nextLine().charAt(0);
+            
+            while (menu != 'y' && menu != 'Y' && menu != 't' && menu != 'T') {
+                System.out.println("Pilihan tidak valid. Silakan masukkan Y/y atau T/t.");
+                System.out.print("Apakah Anda ingin mengulang permainan? (Y/y, T/t): ");
+                menu = input.nextLine().charAt(0);
+            }
+
+        } while (menu == 'y' || menu == 'Y');
+        
+        if (menu == 't' || menu == 'T') {
+            System.out.println("Terima kasih telah bermain!");
+        }
     }
 }
