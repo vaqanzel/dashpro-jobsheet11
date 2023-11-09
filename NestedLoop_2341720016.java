1c7d801efddecf38b234dcb8170946909b6d9c4c
vaqanzel <valengrehasta@gmail.com>
2023-11-10 00:21:48 +0700

    menampilkan array menggunakan foreach

--- NestedLoop_2341720016.java
+++ NestedLoop_2341720016.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
+
 public class NestedLoop_2341720016 {
-    
-   public static void main(String[] args) {
+
+    public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-        Double temps[][] = new Double[5][7];
+            Double temps[][] = new Double[5][7];
 
             for (int i = 0; i < temps.length; i++) {
-            System.out.println("Kota ke-" + i);
+                System.out.println("Kota ke-" + i);
                 for (int j = 0; j < temps[0].length; j++) {
-                 System.out.print("Hari ke-" + (j + 1) + ": ");
+                    System.out.print("Hari ke-" + (j + 1) + ": ");
                     temps[i][j] = scanner.nextDouble();
                 }
                 System.out.println();
             }
+
+            // Menampilkan array menggunakan foreach
             for (int i = 0; i < temps.length; i++) {
                 System.out.print("Kota ke-" + (i + 1) + ": ");
-                for (int j = 0; j < temps[0].length; j++) {
-                    System.out.print(temps[i][j] + " ");
+                for (Double temp : temps[i]) {
+                    System.out.print(temp + " ");
                 }
                 System.out.println();
             }
         }
-
     }
-
 }
